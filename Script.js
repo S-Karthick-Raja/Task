6b09f34ec74e0a25b89c2f9bebd77a4080d2dd6d
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2021-10-12 14:46:52 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,125 @@
+var div = document.createElement('div');
+div.className = 'row';
+
+function Germany() {
+ var div = document.createElement('div');
+div.className = 'class="col-12 col-sm-6 col-md-3"';
+var div = document.createElement('div');
+div.className = 'Flag-Container';
+const img1 = new Image(200, 230);
+img1.src = "https://flagpedia.net/data/flags/w580/de.png";
+document.body.appendChild(img1);
+img1.classList.add("Card-Image-Top");
+
+var div = document.createElement('div');
+div.className = 'Details';
+var para = document.createElement("p");              
+para.innerText = "Country: Germany";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Population: 81,770,900";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Region: Europe";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Capital: Berlin";               
+document.body.appendChild(para);
+}
+Germany()
+ 
+function Australia() {
+  var div = document.createElement('div');
+div.className = 'class="col-12 col-sm-6 col-md-3"';
+var div = document.createElement('div');
+div.className = 'Flag-Container';
+const img2 = new Image(200, 230);
+img2.src = "https://flagpedia.net/data/flags/w580/au.png";
+document.body.appendChild(img2);
+img2.classList.add("Card-Image-Top");
+
+var div = document.createElement('div');
+var para = document.createElement("p");              
+para.innerText = "Country: Australia";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Population: 91,657,312";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Region: Aus";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Capital: Canberra";               
+document.body.appendChild(para);
+}
+Australia()
+
+function India() {
+  var div = document.createElement('div');
+div.className = 'class="col-12 col-sm-6 col-md-3"';
+var div = document.createElement('div');
+div.className = 'Flag-Container';
+const img3 = new Image(200, 230);
+img3.src = "https://flagpedia.net/data/flags/w580/in.png";
+document.body.appendChild(img3);
+img3.classList.add("Card-Image-Top");
+
+var div = document.createElement('div');
+var para = document.createElement("p");              
+para.innerText = "Country: India";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Population: 1,3179,984,812";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Region: Asia";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Capital: New Delhi";               
+document.body.appendChild(para);
+}
+India();
+
+function SK() {
+  var div = document.createElement('div');
+div.className = 'class="col-12 col-sm-6 col-md-3"';
+var div = document.createElement('div');
+div.className = 'Flag-Container';
+const img4 = new Image(200, 230);
+img4.src = "https://flagpedia.net/data/flags/w580/kr.png";
+document.body.appendChild(img4);
+img4.classList.add("Card-Image-Top");
+
+var div = document.createElement('div');
+var para = document.createElement("p");              
+para.innerText = "Country: South Korea";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Population: 40,400,000";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Region: Asia";               
+document.body.appendChild(para); 
+
+var para = document.createElement("p");              
+para.innerText = "Capital: Seoul";               
+document.body.appendChild(para);
+}
+SK();
+
+
+
+
+
+  
